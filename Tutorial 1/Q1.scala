3576236faf5b4f162195b83f15406756fc62a45a
Sanjeewa Madushan YAPA <133627006+AquaKloud@users.noreply.github.com>
2023-10-29 13:58:08 +0530

    Update Q1.scala

--- Tutorial 1/Q1.scala
+++ Tutorial 1/Q1.scala
@@ -1,1 +1,13 @@
+object Q1{
 
+def main(args:Array[String]): Unit = {
+
+  Area_Of_Disk(5.2)
+}
+  def Area_of_Disk(radius:Double): Unit = {
+
+    val Area=radius*radius*22/7
+    println(s"Area of the disk is = $Area")
+  }
+  
+}
