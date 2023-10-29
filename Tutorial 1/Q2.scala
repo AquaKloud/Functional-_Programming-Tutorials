26c01a1d2472cc63c679b999a5efb35fd54ba174
Sanjeewa Madushan YAPA <133627006+AquaKloud@users.noreply.github.com>
2023-10-29 13:59:38 +0530

    Update Q2.scala

--- Tutorial 1/Q2.scala
+++ Tutorial 1/Q2.scala
@@ -1,1 +1,13 @@
+object CelsiusToFahrenheit {
+  def main(args: Array[String]): Unit = {
+    // Define the temperature in Celsius
+    val celsius = 35.0
+
+    // Convert to Fahrenheit
+    val fahrenheit = celsius * 1.8 + 32.0
+
+    // Print the result
+    println(s"$celsius degrees Celsius is equal to $fahrenheit degrees Fahrenheit")
+  }
+}
 
