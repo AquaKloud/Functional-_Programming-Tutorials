88db127bfc695075f96a020773e08d6df47e9194
Sanjeewa Madushan YAPA <133627006+AquaKloud@users.noreply.github.com>
2023-10-29 14:00:34 +0530

    Update Q3.scala

--- Tutorial 1/Q3.scala
+++ Tutorial 1/Q3.scala
@@ -1,1 +1,13 @@
+object CalculateSphereVolume {
+  def main(args: Array[String]): Unit = {
+    // Define the radius
+    val radius = 5.0
+
+    // Calculate the volume
+    val volume = (4.0/3.0) * math.Pi * math.pow(radius, 3)
+
+    // Print the result
+    println(s"The volume of a sphere with radius $radius is $volume")
+  }
+}
 
