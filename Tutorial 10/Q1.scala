ff54f1d5c5b7e1adaeea5fecc40c77fb2a05644d
Sanjeewa Madushan YAPA <133627006+AquaKloud@users.noreply.github.com>
2023-10-29 14:55:34 +0530

    Update Q1.scala

--- Tutorial 10/Q1.scala
+++ Tutorial 10/Q1.scala
@@ -1,1 +1,28 @@
+object Q_1 {
+  def main(args: Array[String]): Unit = {
+    println("Enter temperatures in celsius (one per line)\npress Enter on an empty line to finish")
 
+    val userInputList = readInputs()
+    println("User input list: " + userInputList)
+
+    println("Average Fahrenheit temperature: " + calculateAverage(userInputList) + "F")
+  }
+
+  def readInputs(): List[Double] = {
+    val input = scala.io.StdIn.readLine()
+    if (input.isEmpty) {
+      Nil
+    }
+    else {
+      val temp = input.toDouble
+      temp :: readInputs()
+    }
+  }
+
+  def calculateAverage(temperatureCelsius : List[Double]): Double = {
+    val temperatureFahrenheit = temperatureCelsius.map(c => c * (9 / 5) + 32)
+    val sum = temperatureFahrenheit.reduce((a, b) => a + b)
+
+    sum / temperatureFahrenheit.length
+  }
+}
