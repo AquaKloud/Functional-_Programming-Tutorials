4a57be87cdc58bf2a7cf525d7327388a2697e82b
Sanjeewa Madushan YAPA <133627006+AquaKloud@users.noreply.github.com>
2023-10-29 14:52:31 +0530

    Update Q1.scala

--- Tutorial 9/Q1.scala
+++ Tutorial 9/Q1.scala
@@ -1,1 +1,19 @@
+object Q1 {
+  def main(args: Array[String]): Unit = {
+    var x= new Rational(1,4)
+    println("Rational Number : "+x)
+
+    var y = x.neg
+    println("Negative Value : " + y)
+  }
+  class Rational(n:Int, d:Int){
+    require(d != 0,"Denominator can not be zero")
+
+    def numerator =n
+    def denominator =d
+
+    def neg = new Rational(-this.numerator,this.denominator)
+    override def toString():String= numerator + "/" + denominator
+  }
+}
 
