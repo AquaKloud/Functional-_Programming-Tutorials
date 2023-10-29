bfc78c33e5e4040156b24850b485b0cc5c9d0899
Sanjeewa Madushan YAPA <133627006+AquaKloud@users.noreply.github.com>
2023-10-29 14:56:25 +0530

    Update Q2.scala

--- Tutorial 10/Q2.scala
+++ Tutorial 10/Q2.scala
@@ -1,1 +1,27 @@
+object Q2 {
+  def main(args: Array[String]): Unit = {
+    println("Enter words(one per line)\npress Enter on an empty line to finish")
 
+    val userInputList = readInputs()
+    println("User input list: " + userInputList)
+
+    println("Total count of letter occurrences: " +countLetterOccurrences(userInputList))
+  }
+
+  def readInputs(): List[String] = {
+    val input = scala.io.StdIn.readLine()
+    if (input.isEmpty) {
+      Nil
+    }
+    else {
+      input :: readInputs()
+    }
+  }
+
+  def countLetterOccurrences(words: List[String]): Int = {
+    val wordLength = words.map(_.length)
+    val total = wordLength.reduce(_+_)
+
+    total
+  }
+}
