7036cd6226fd4eb75f89b6d24d91ccab08b64720
Sanjeewa Madushan YAPA <133627006+AquaKloud@users.noreply.github.com>
2023-10-29 14:25:12 +0530

    Update Q3.scala

--- Tutorial 5/Q3.scala
+++ Tutorial 5/Q3.scala
@@ -1,1 +1,17 @@
+object Q3 {
+  def main(args: Array[String]): Unit = {
+    print("Enter your number - ")
+    val number = scala.io.StdIn.readInt()
 
+    println(s"Addition of numbers from 1 to ${number} - " +sum(number))
+  }
+
+  def sum(number: Int): Int = {
+    if (number == 1){
+      1
+    }
+    else{
+      number + sum(number - 1)
+    }
+  }
+}
